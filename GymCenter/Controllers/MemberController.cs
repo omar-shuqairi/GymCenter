3bdbdec4c61b4beebcf0ab515e3395df08906019
Bolbol Beek <omarshuqairi@hotmail.com>
2025-01-14 10:55:07 +0300

    setup contact and services pages data from db

--- GymCenter/Controllers/MemberController.cs
+++ GymCenter/Controllers/MemberController.cs
@@ -9,13 +9,14 @@ namespace GymCenter.Controllers
     {
         private readonly ModelContext _context;
         private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly EmailService _emailService;
 
 
-        public MemberController(ModelContext context, IWebHostEnvironment webHostEnvironment)
+        public MemberController(ModelContext context, IWebHostEnvironment webHostEnvironment, EmailService emailService)
         {
             _context = context;
             _webHostEnvironment = webHostEnvironment;
-
+            _emailService = emailService;
         }
         public async Task<IActionResult> Home()
         {
@@ -39,9 +40,16 @@ namespace GymCenter.Controllers
             return View(model);
         }
 
-        public IActionResult Services()
+
+        public async Task<IActionResult> Services()
         {
-            return View();
+
+            var workoutplans = await _context.Workoutplans.ToListAsync();
+            var shredimg = await _context.Siteinfos
+            .Select(s => s.SharedImagePath)
+            .FirstOrDefaultAsync();
+            var model = Tuple.Create(workoutplans, shredimg);
+            return View(model);
         }
 
         public IActionResult Testimonials()
@@ -49,9 +57,39 @@ namespace GymCenter.Controllers
             return View();
         }
 
-        public IActionResult Contact()
+        public async Task<IActionResult> Contact()
         {
-            return View();
+            var shredimg = await _context.Siteinfos
+            .Select(s => s.SharedImagePath)
+            .FirstOrDefaultAsync();
+
+            var contactinfo = await _context.Contactus.FirstOrDefaultAsync();
+
+            var model = Tuple.Create(contactinfo, shredimg);
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Contact([Bind("Guestname,Guestemail,Guestcomment")] Contactform contactform)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contactform);
+                await _context.SaveChangesAsync();
+
+                // Compose an email
+                string subject = "Thank you for contacting us!";
+                string body = $@"
+                 <p>Dear {contactform.Guestname},</p>
+                 <p>Thank you for reaching out to us. We have received your message:</p>
+                 <blockquote>{contactform.Guestcomment}</blockquote>
+                 <p>Our team will get back to you as soon as possible.</p>
+                 <p>Best regards,</p>
+                 <p>Your GymCenter Team</p>";
+                await _emailService.SendEmailAsync(contactform.Guestemail, subject, body);
+                return RedirectToAction(nameof(Contact));
+            }
+            return View(contactform);
         }
 
         public IActionResult Profile()
