42490d8b580336e72e483e8f7e041e2843a6f37d
Bolbol Beek <omarshuqairi@hotmail.com>
2025-01-18 15:35:39 +0300

    test puch

--- GymCenter/Controllers/TrainerController.cs
+++ GymCenter/Controllers/TrainerController.cs
@@ -25,7 +25,6 @@ namespace GymCenter.Controllers
             .Include(m => m.Plan)
             .AsNoTracking()
             .ToListAsync();
-
             return View(membersWithDetails);
 
         }
